745d5467f6622ecdd20b50369ec72ad3cbfb6daf
Marco Foscato <marco.foscato@uib.no>
2023-03-06 12:40:45 +0100

    conflicting use of socket

--- JsonSocketClient/src/main/java/jsonclient/Client.java
+++ JsonSocketClient/src/main/java/jsonclient/Client.java
@@ -3,55 +3,126 @@ package jsonclient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
+	
+	 Gson jsonConverter;
+	 String clientID;
+	 List<String> allSmiles = Arrays.asList("c_", 
+			"",
+			"cc_",
+			"ccc_",
+			"cccc_",
+			"ccccc_",
+			"cccccc_",
+			"ccccccc_",
+			"cccccccc_",
+			"ccccccccc_",
+			"cccccccccc_");
+	
+	 Socket socket;
+	 PrintWriter writerToSocket;
+	 BufferedReader readerFromSocket;
+	
+	private  List<Task> submitted;
+    private  ThreadPoolExecutor tpe;
+    private  Map<Task,Future<Object>> futures;
+    
     public static void main(String[] args) throws java.io.IOException 
     {	
-    	Gson jsonConverter = new GsonBuilder()
+    	Client c = new Client();
+    	c.initialize(args);
+    	c.run();
+    	c.terminate();
+    }
+    
+    public void initialize(String[] args) throws UnknownHostException, IOException 
+    {
+    	jsonConverter = new GsonBuilder()
     	        .create();
     	
-    	String clientID = "Client"+System.currentTimeMillis();
+    	clientID = "Client"+System.currentTimeMillis();
     	if (args.length==1)
     		clientID = args[0];
-    		
-    	List<String> allSmiles = Arrays.asList("c_", 
-    			"",
-    			"cc_",
-    			"ccc_",
-    			"cccc_",
-    			"ccccc_",
-    			"cccccc_",
-    			"ccccccc_",
-    			"cccccccc_",
-    			"ccccccccc_",
-    			"cccccccccc_");
-    			
+    	
+    	futures = new HashMap<Task,Future<Object>>();
+        submitted = new ArrayList<Task>();
+    	tpe = new ThreadPoolExecutor(4, 4, 0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(1));
+    	
     	/*
-    	 * We'll need shotdown hook 
+    	 * We'll need shotdown hook to close the socket
     	 * see https://stackoverflow.com/questions/8051863/how-can-i-close-the-socket-in-a-proper-way
     	 */
 
-        Socket socket = new Socket("localhost", 0xf17);
+        socket = new Socket("localhost", 0xf17);
         OutputStream outputSocket = socket.getOutputStream();
-        PrintWriter writerToSocket = new PrintWriter(outputSocket, true);
+        writerToSocket = new PrintWriter(outputSocket, true);
     	
         InputStream inputFromSocket = socket.getInputStream();
-        BufferedReader readerFromSocket = new BufferedReader(
+        readerFromSocket = new BufferedReader(
         		new InputStreamReader(inputFromSocket));
-        
+    }
+    
+    private void run()
+    {   
+        for (int i=0; i<4; i++)
+        {
+        	Task task = new Task("Tsk"+i);
+        	submitted.add(task);
+            futures.put(task, tpe.submit(task));
+        }
+    }
+    
+    public void terminate() throws IOException {
+        socket.close();
+    }
+    
+//------------------------------------------------------------------------------
+    
+    /*
+     * A single-thread task asking for fitness independently on any other thread
+     */
+    public class Task implements Callable<Object>
+    {
+    	private String name = "noname";
+    	public Task(String name) {
+    		this.name = name;
+    	}
+
+		public Object call() throws Exception {
+			keepAskingForFitness(name);
+			return null;
+		}
+    	
+    }
+    
+    private void keepAskingForFitness(String threadID)
+    {
         boolean goon= true;
         int i = 0;
         while (goon)
@@ -67,24 +138,40 @@ public class Client {
             
 	        JsonObject jsonObj = new JsonObject();
 	        jsonObj.addProperty("SMILES", smiles);
-	        jsonObj.addProperty("Client", clientID);
+	        jsonObj.addProperty("Client", threadID);
 	        
 	        writerToSocket.println(jsonConverter.toJson(jsonObj));
 	        
-	        JsonObject answer = jsonConverter.fromJson(
-	        		readerFromSocket.readLine(), JsonObject.class);
+	        JsonObject answer = null;
+			try {
+				answer = jsonConverter.fromJson(
+						readerFromSocket.readLine(), JsonObject.class);
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.exit(-1);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.exit(-1);
+			}
 	        
 	        String val = "none";
 	        if (answer.has("FITNESS"))
 	        	val = String.format("%.2f", Double.parseDouble(answer.get("FITNESS").toString()));
-	        	
-            System.out.println("--> "+answer.get("Client")+" val:" + val);
-        	
+
+            String taskIdFromServer = answer.get("Client").getAsString();	
+	        
+            System.out.println("--> "+taskIdFromServer+" val:" + val);
+            if (!threadID.equals(taskIdFromServer))
+            {
+            	System.out.println("Stopping because "+threadID+"!="+taskIdFromServer);
+            	System.exit(-1);
+            }
+            
         	// This will never be satisfied
         	if (answer.equals("NOT_POSSIBLE"))
         		goon = false;
         }
-        
-        socket.close();
     }
 }
